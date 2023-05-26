d553b8f7eb9a4515eab34648f993cb6926fe3a94
Willcley Melo <willcley2003@gmail.com>
2023-05-26 17:32:45 -0300

    add: estilização e modal

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,23 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handlerModal() {
+    const modal = document.querySelector('.modal__controller');
+    const modalButtons = document.querySelectorAll('.btn__modal');
+
+    for (let i = 0; i < modalButtons.length; i++) {
+        modalButtons[i].addEventListener('click', () => {
+            modal.showModal();
+
+            closeModal();
+        })
+    }
+}
+
+function closeModal() {
+    const modal = document.querySelector('.modal__controller');
+    const buttonClose = document.querySelector('.btn__modal--close');
+
+    buttonClose.addEventListener('click', () => {
+        modal.close();
+    })
+}
+
+handlerModal();
